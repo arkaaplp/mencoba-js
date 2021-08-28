b4b87e82624d2cce20914a5609818a35fcc1d806
Your name here <arkarezky540@gmail.com>
2021-08-28 14:27:49 +0800

    mencoba js cinn

---
+++ index.js
@@ -0,0 +1,11 @@
+prompt("kabarnya gimana ?")
+
+prompt("lagi ngapain aja hari ini ?")
+
+alert("lagi pusing nugas kah ?")
+
+alert("wqwq sama, lagi pusing juga nih kebanyakan tugas")
+
+alert("tapi lebih pusing lagi kalau ngk ada kamu")
+
+alert("awokawokawkawk , coba2 ji ini cuk , kerenji ngk")
